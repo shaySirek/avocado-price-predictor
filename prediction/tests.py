ffc19152e599c6d23e1a236037d4190ab90265cf
shaySirek <shaysirek@control-plane.minikube.internal>
2021-06-08 10:36:02 +0300

    change tests to pytest-django

--- prediction/tests.py
+++ prediction/tests.py
@@ -1,41 +1,58 @@
+import pytest
 from django.urls import reverse
-from django.test import TestCase, Client
 
 
-class PredictViewTests(TestCase):
-    def test_method_not_allowed(self):
-        response = self.client.get(reverse('predict'))
-        self.assertEqual(response.status_code, 405)
-        
-    def test_empty(self):
-        response = self.client.post(reverse('predict'))
-        self.assertEqual(response.status_code, 400)
+@pytest.mark.django_db
+def test_method_not_allowed(client):
+    response = client.get(reverse('predict'))
+    assert response.status_code == 405 
+    
+@pytest.mark.django_db
+def test_empty(client):
+    response = client.post(reverse('predict'))
+    assert response.status_code == 400
 
-    def test_invalid(self):
-        data = {
-            'sold_plu_4046' : 'not number',
-            'sold_plu_4225' : 6,
-            'sold_plu_4770': 8,
-            'small_bags': 4,
-            'large_bags': 0,
-            'xlarge_bags': 0,
-            'organic': 'on',
-            'region': 'Albany'
-        }
-        response = self.client.post(reverse('predict'), data)
-        self.assertEqual(response.status_code, 400)
+@pytest.mark.django_db
+def test_invalid_data(client):
+    data = {
+        'sold_plu_4046' : 'not number',
+        'sold_plu_4225' : 6,
+        'sold_plu_4770': 8,
+        'small_bags': 4,
+        'large_bags': 0,
+        'xlarge_bags': 0,
+        'organic': 'on',
+        'region': 'Albany'
+    }
+    response = client.post(reverse('predict'), data)
+    assert response.status_code == 400
 
-    def test_valid(self):
-        data = {
-            'sold_plu_4046' : 5,
-            'sold_plu_4225' : 6,
-            'sold_plu_4770': 8,
-            'small_bags': 4,
-            'large_bags': 0,
-            'xlarge_bags': 0,
-            'organic': 'on',
-            'region': 'Albany'
-        }
-        response = self.client.post(reverse('predict'), data)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "1019")
+@pytest.mark.django_db
+def test_missing_field(client):
+    data = {
+        'sold_plu_4225' : 6,
+        'sold_plu_4770': 8,
+        'small_bags': 4,
+        'large_bags': 0,
+        'xlarge_bags': 0,
+        'organic': 'on',
+        'region': 'Albany'
+    }
+    response = client.post(reverse('predict'), data)
+    assert response.status_code == 400
+
+@pytest.mark.django_db
+def test_valid(client):
+    data = {
+        'sold_plu_4046' : 5,
+        'sold_plu_4225' : 6,
+        'sold_plu_4770': 8,
+        'small_bags': 4,
+        'large_bags': 0,
+        'xlarge_bags': 0,
+        'organic': 'on',
+        'region': 'Albany'
+    }
+    response = client.post(reverse('predict'), data)
+    assert response.status_code == 200
+    assert "1019" in response.content.decode('utf-8')
