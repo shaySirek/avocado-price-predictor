dfe57d4b33cc475f72f0bc34f0325501aa6947d2
shaySirek <shaysirek@gmail.com>
2021-06-28 16:35:37 +0300

    secure Django settings

--- prediction/settings.py
+++ prediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v(ctt&%p#$)3avx=(ibw-0e=^k#&7rwpnd769@9e5ub220kstx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [environ.get('HOST', '0.0.0.0')]
 
 # Application definition
 
