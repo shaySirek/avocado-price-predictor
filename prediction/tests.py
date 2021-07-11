70bd4df0984852bd5ac3be24ef0e9ac7b4abdb8f
Shay Sirek <shaysirek@gmail.com>
2021-07-11 11:08:41 +0300

    add encode/decode functions in tests
    Signed-off-by: Shay Sirek <shaysirek@gmail.com>

--- prediction/tests.py
+++ prediction/tests.py
@@ -17,6 +17,12 @@ def data():
         'region': 'Albany'
     }
 
+def encode(data):
+    return dumps(data).encode('utf-8')
+
+def decode(data):
+    return loads(data.decode('utf-8'))
+
 def test_price_prediction_method_not_allowed(client):
     response = client.get(reverse('predict'))
     assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
@@ -24,26 +30,26 @@ def test_price_prediction_method_not_allowed(client):
 def test_price_prediction_empty(client):
     data = dict()
     
-    response = client.post(reverse('predict'), dumps(data).encode('utf-8'), content_type="application/json")
+    response = client.post(reverse('predict'), encode(data), content_type="application/json")
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 def test_price_prediction_invalid_data(client, data):
     data['sold_plu_4046'] = 'not number'
 
-    response = client.post(reverse('predict'), dumps(data).encode('utf-8'), content_type="application/json")
+    response = client.post(reverse('predict'), encode(data), content_type="application/json")
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 def test_price_prediction_missing_field(client, data):
     del data['sold_plu_4046']
 
-    response = client.post(reverse('predict'), dumps(data).encode('utf-8'), content_type="application/json")
+    response = client.post(reverse('predict'), encode(data), content_type="application/json")
     assert response.status_code == HTTPStatus.BAD_REQUEST
 
 def test_price_prediction_valid(client, data):
-    response = client.post(reverse('predict'), dumps(data).encode('utf-8'), content_type="application/json")
+    response = client.post(reverse('predict'), encode(data), content_type="application/json")
     assert response.status_code == HTTPStatus.OK
 
-    content = loads(response.content.decode('utf-8'))
+    content = decode(response.content)
     assert 'predicted_average_price' in content.keys()
    
 def test_default_method_not_allowed(client):
@@ -54,6 +60,6 @@ def test_default_valid(client, data):
     response = client.get(reverse('default'))
     assert response.status_code == HTTPStatus.OK
 
-    content = loads(response.content.decode('utf-8'))
+    content = decode(response.content)
     assert data.keys() == content.keys()
     assert all(len(v) == 0 for v in content.values())
